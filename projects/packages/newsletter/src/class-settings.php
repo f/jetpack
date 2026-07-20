c07a6948d757086a5deb5064a2dcd6c716ed3ba2
Dave Martin <dave.martin@automattic.com>
2026-07-20 11:50:50 -0400

    The Mode flag

--- projects/packages/newsletter/src/class-settings.php
+++ projects/packages/newsletter/src/class-settings.php
@@ -289,6 +289,7 @@ class Settings {
 			'setupPaymentPlansUrl'            => $setup_payment_plan_url,
 			'isSitePublic'                    => ! $status->is_private_site() && ! $status->is_coming_soon(),
 			'tracksUserData'                  => Jetpack_Tracks_Client::get_connected_user_tracks_identity(),
+			'modeEnabled'                     => Mode::is_enabled(),
 		);
 
 		return $data;
